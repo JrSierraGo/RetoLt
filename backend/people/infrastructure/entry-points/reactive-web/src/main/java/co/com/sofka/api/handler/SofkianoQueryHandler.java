31cf1dca539c641a0505fc1db89679705f53190e
Junior Sierra Gomez <junior_sierra97@outlook.com>
2023-07-18 21:18:17 -0500

    SofkianoQueryHandler remove unnecessary implementation

--- backend/people/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/handler/SofkianoQueryHandler.java
+++ backend/people/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/handler/SofkianoQueryHandler.java
@@ -1,28 +1,20 @@
 package co.com.sofka.api.handler;
 
-import co.com.sofka.api.dto.SofkianoDTO;
-import co.com.sofka.domainconverter.DomainMapper;
-import co.com.sofka.model.sofkiano.Sofkiano;
 import co.com.sofka.model.sofkiano.SofkianoPageable;
 import co.com.sofka.usecase.handler.SofkianoHandlerUseCase;
-import org.reactivecommons.utils.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/api/ms-people/sofkiano/query")
-public class SofkianoQueryHandler extends DomainMapper<Sofkiano, SofkianoDTO> {
+public class SofkianoQueryHandler {
 
     private final SofkianoHandlerUseCase useCase;
-
-    protected SofkianoQueryHandler(ObjectMapper mapper, SofkianoHandlerUseCase useCase) {
-        super(mapper);
-        this.useCase = useCase;
-    }
 
     @GetMapping(
             path = "/all",
